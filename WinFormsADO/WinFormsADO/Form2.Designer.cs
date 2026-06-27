2e601e88f96241f3c2c32ded5020507658ee9eed
BalkesVedat <80820828+BalkesVedat@users.noreply.github.com>
2026-06-27 15:23:55 +0300

    27062026_1523

---
+++ WinFormsADO/WinFormsADO/Form2.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WinFormsADO
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgwPasifListesi = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwPasifListesi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgwPasifListesi
+            // 
+            this.dgwPasifListesi.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dgwPasifListesi.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dgwPasifListesi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwPasifListesi.Location = new System.Drawing.Point(31, 170);
+            this.dgwPasifListesi.Name = "dgwPasifListesi";
+            this.dgwPasifListesi.RowHeadersWidth = 102;
+            this.dgwPasifListesi.RowTemplate.Height = 40;
+            this.dgwPasifListesi.Size = new System.Drawing.Size(1844, 669);
+            this.dgwPasifListesi.TabIndex = 0;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1930, 867);
+            this.Controls.Add(this.dgwPasifListesi);
+            this.Name = "Form2";
+            this.Text = "Pasif Müşteri Listesi";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwPasifListesi)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwPasifListesi;
+    }
+}
---
+++ WinFormsADO/WinFormsADO/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsADO
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=HastakayitDB;Persist Security Info=True;User ID=sa;Password=1");
+
+        SqlCommand cmdSelect = new SqlCommand("SELECT HastaID, AdSoyad, Adres, Telefon, Sikayetler, Tarih, AktifMi, BorcTutari FROM Hastalar WHERE AktifMi = 0");
+
+        SqlDataAdapter adapter = new SqlDataAdapter();
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.Height = 400;
+            this.Width = 750;
+
+            ListeyiYenile();
+        }
+
+        private void ListeyiYenile()
+        {
+            cmdSelect.Connection = conn;
+            adapter.SelectCommand = cmdSelect;
+
+            DataTable dt = new DataTable();
+
+            adapter.Fill(dt);
+
+            dgwPasifListesi.DataSource = dt;
+        }
+
+
+    }
+}
